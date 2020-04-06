7b0aa576ffc9df7f4912e4d6ef1710da252d4ec0
EliVerhulst <eli.verhulst4@gmail.com>
2020-04-06 14:37:43 +0200

    added project again, wrote basics of program

---
+++ Trime2/app/src/main/java/com/example/trime/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.trime
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.time.LocalDate
+import android.view.View;
+import android.widget.TimePicker;
+import android.app.TimePickerDialog
+import android.app.DatePickerDialog
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    var timeFormat = SimpleDateFormat("HH:mm")       //zet het standaardformaat voor een datum
+    var formate = SimpleDateFormat("dd MMM, YYYY")   //zet het standaardformaat voor tij
+    private val timePicker1: TimePicker? = null             // we maken een timepicker aan
+    var datum = Date("01/01/2020")
+    var uur = Date("01/01/2020")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val timeButton = findViewById<Button>(R.id.timeButton);
+        val confirm = findViewById<Button>(R.id.confirm);
+        val test1 = findViewById<TextView>(R.id.Test1);
+        val test2 = findViewById<TextView>(R.id.Test2);
+        timeButton.setOnClickListener{              //onclicklistener initieren: iets doen wanneer er op de knop gedrukt wordt
+            val now = Calendar.getInstance();       // steken de huidige tijd in een variabele
+
+            val datepicker = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view, year, month, dayOfMonth ->
+                val selectedDate = Calendar.getInstance()
+                selectedDate.set(Calendar.YEAR,year)
+                selectedDate.set(Calendar.MONTH,month)
+                selectedDate.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+                val date = (selectedDate.time)
+                datum = selectedDate.time
+                //de datepicker opent, de gekozen tijd wordt in een variabele gestoken: selectedDate. In date wordt er een datum van gemaakt.
+
+            },
+                    now.get(Calendar.YEAR), now.get(Calendar.MONTH),now.get(Calendar.DAY_OF_MONTH))
+            datepicker.show();
+            try {
+                if(timeButton.text != "Kies Alarm") {
+                    val date = timeFormat.parse(timeButton.text.toString())
+
+                    now.time = date
+                }
+            }catch (e:Exception){
+                e.printStackTrace()}
+            val timepicker = TimePickerDialog(this, TimePickerDialog.OnTimeSetListener{view, hourOfDay, minute ->
+                val selectedTime = Calendar.getInstance()
+                selectedTime.set(Calendar.HOUR_OF_DAY,hourOfDay)
+                selectedTime.set(Calendar.MINUTE, minute)
+                val time = (selectedTime.time)
+                uur = time
+
+            },
+                    now.get(Calendar.HOUR_OF_DAY),now.get(Calendar.MINUTE),true)
+            timepicker.show()
+
+        }
+        confirm.setOnClickListener{
+            test1.text = formate.format(datum)
+            test2.text = timeFormat.format(uur)
+        }
+
+    }
+}
