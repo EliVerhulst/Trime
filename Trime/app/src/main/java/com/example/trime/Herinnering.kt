e7b9537e1f498e072f941379b09a7b15b1f2ad68
EliVerhulst <eli.verhulst4@gmail.com>
2020-04-06 13:19:13 +0200

    Nieuw project gestart

---
+++ Trime/app/src/main/java/com/example/trime/Herinnering.kt
@@ -0,0 +1,11 @@
+package com.example.trime
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.Month
+
+
+class Herinnering {
+   // var date = calender.
+
+}
